cf8a7f2985f9c8dc05c4f9dec6e9cdcea9d39f8a
Victor Candeira <vscandeira@gmail.com>
2023-09-27 09:37:34 -0300

    Funções básicas implementadas. Próximos passos: implementar tratamento de erro na conversão para o caso do usuário não informar valor, implementar tratamento de erro no caso de divisão por zero, só permitir valores positivos, trazer prints para classe IO, implementar testes

--- bin/imc_calc.dart
+++ bin/imc_calc.dart
@@ -4,5 +4,8 @@ import 'package:imc_calc/io.dart';
 void main(List<String> arguments) {
   var pessoa = Pessoa();
   print('Olá! Bem vindo à calculadora IMC. Peço que digite sua altura: ');
+  pessoa.setAltura(IO.readDouble());
   print('Agora seu peso: ');
+  pessoa.setPeso(IO.readDouble());
+  print('Seu IMC é: ${pessoa.getIMC().toStringAsFixed(2)}');
 }
--- lib/io.dart
+++ lib/io.dart
@@ -1,1 +1,9 @@
+import 'dart:io';
+import 'dart:convert';
 
+class IO {
+  static double readDouble() {
+    var value = stdin.readLineSync(encoding: utf8) ?? "";
+    return double.parse(value);
+  }
+}
