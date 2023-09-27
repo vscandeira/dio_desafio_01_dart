99a79ca449872d4daf47f8cfb70ed411c7ae3115
Victor Candeira <vscandeira@gmail.com>
2023-09-27 10:19:22 -0300

    tratamento de exceção de entrada inválida (caracter não numérico). falta: número negativo.

--- bin/imc_calc.dart
+++ bin/imc_calc.dart
@@ -3,9 +3,12 @@ import 'package:imc_calc/io.dart';
 
 void main(List<String> arguments) {
   var pessoa = Pessoa();
-  print('Olá! Bem vindo à calculadora IMC. Peço que digite sua altura: ');
+  IO.printLine(
+      '####################################################################################################\n\nOlá! Bem vindo à calculadora IMC. Peço que digite sua altura: ');
   pessoa.setAltura(IO.readDouble());
-  print('Agora seu peso: ');
+  IO.printLine('Agora seu peso: ');
   pessoa.setPeso(IO.readDouble());
-  print('Seu IMC é: ${pessoa.getIMC().toStringAsFixed(2)}');
+  IO.printLineDouble('Seu IMC é: ', pessoa.getIMC());
+  IO.printLine(
+      '\n\n####################################################################################################');
 }
--- lib/io.dart
+++ lib/io.dart
@@ -4,6 +4,19 @@ import 'dart:convert';
 class IO {
   static double readDouble() {
     var value = stdin.readLineSync(encoding: utf8) ?? "";
-    return double.parse(value);
+    try {
+      return double.parse(value);
+    } on FormatException {
+      printLine('Formato inválido de dado, será atribuído valor 0.');
+      return 0.0;
+    }
+  }
+
+  static void printLine(String msg) {
+    print(msg);
+  }
+
+  static void printLineDouble(String msg, double x) {
+    printLine(msg + x.toStringAsFixed(2));
   }
 }
